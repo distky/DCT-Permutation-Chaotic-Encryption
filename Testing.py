f3d478c818ec014e8cbf6b2ec267059c9cc26b8b
Andrean Lius <andreanlius@gmail.com>
2021-08-24 15:19:35 +0700

    clahe

--- Testing.py
+++ Testing.py
@@ -59,6 +59,22 @@ randomMessage = rounding(np.random.rand(2,2) * 255,0).astype('uint8')
 
 randomCover = rounding(np.random.rand(32,32) * 255, 0).astype('uint8')
 
+def CLAHE(path1):
+    img = openImageFromPath(path1)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    cl1 = clahe.apply(img)
+    cl1 = cl1.astype('uint8')
+
+def clahe():
+    citraSampul = openImageFromPath('D:/DCT_Permutation_Skripsi/DCT-Permutation-Chaotic-Encryption/lena_gray.jpeg')
+    
+    citraSampul = bgr2gray(citraSampul)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    citraSampul= clahe.apply(citraSampul)
+    saveImageAs(citraSampul.astype('uint8'), 'D:/DCT_Permutation_Skripsi/DCT-Permutation-Chaotic-Encryption/lena_gray-contrast.jpeg')
+    
+
+clahe()
 # print(randomMessage)
 
 # dct = discreteCosineTransform(randomMessage)
@@ -77,33 +93,34 @@ randomCover = rounding(np.random.rand(32,32) * 255, 0).astype('uint8')
 # print(inverseDct[0][0])
 # print(idct3[0][0])
 
-print(math.pi)
+# print(math.pi)
 
-print("==Source==")
-print(randomMessage)
-print(randomCover)
+# print("==Source==")
+# print(randomMessage)
+# print(randomCover)
 
-encryptedRandomMessage = encryption(randomMessage)
+# encryptedRandomMessage = encryption(randomMessage)
 
-print('==Encrypted==')
-print(encryptedRandomMessage)
+# print('==Encrypted==')
+# print(encryptedRandomMessage)
 
-stegoImage, dcMatrix = steganography(randomCover, encryptedRandomMessage)
+# stegoImage, dcMatrix = steganography(randomCover, encryptedRandomMessage)
 
-print('==After stego==')
-print(stegoImage)
-print(dcMatrix)
+# print('==After stego==')
+# print(stegoImage)
+# print(dcMatrix)
 
-extractedImage, _ = extraction(stegoImage, dcMatrix)
+# extractedImage, _ = extraction(stegoImage, dcMatrix)
 
-print('==After extract==')
-print(extractedImage)
+# print('==After extract==')
+# print(extractedImage)
 
-decryptedImage = decryption(extractedImage)
+# decryptedImage = decryption(extractedImage)
 
-print('==After decrypt==')
-print(decryptedImage)
+# print('==After decrypt==')
+# print(decryptedImage)
 
 # template_matching('F:/Pictures/Testing/result arctichare 32 x 32.tiff', 'F:/Pictures/Pengujian citra pesan/arctichare 32 x 32.tiff')
 # equalizeHistogram('D:/Projects/Python Projects/DCT-Permutation-Chaotic-Encryption/test.tif', 255)
-# saveImageAs(sp_noise(bgr2gray(openImageFromPath('D:/Projects/Python Projects/DCT-Permutation-Chaotic-Encryption/test.tif')), 0.01), 'salted_image.tiff')
+#saveImageAs(sp_noise(bgr2gray(openImageFromPath('D:/Projects/Python Projects/DCT-Permutation-Chaotic-Encryption/test.tif')), 0.01), 'salted_image.tiff')
+#saveImageAs(CLAHE(bgr2gray(openImageFromPath('D:/DCT_Permutation_Skripsi/DCT-Permutation-Chaotic-Encryption/lena_gray.jpeg'))), 'contrast.tiff')
